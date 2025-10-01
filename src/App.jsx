71711a3b518ed7e698c5f713d514013580025641
LucasOlivera615 <lo.lucasolivera@gmail.com>
2025-10-01 12:02:16 -0300

    añadido de bootstrap y añadido de data carpeta data para simular una petición.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import NavBar from './Components/NavBar/NavBar'
 import './App.css'
 import ItemListContainer from './Components/ItemListContainer/ItemListContainer'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
@@ -8,9 +9,18 @@ function App() {
 
   return (
     <>
-      <h1>GyL Importaciones</h1>
-      <NavBar />
-      <ItemListContainer saludo={"Bienvenidos a mi ecommerce, GyL Importaciones"}/>
+      <header>
+        <h1>GyL Importaciones</h1>
+        <div className='col-12'>
+          <NavBar />
+        </div>
+      </header>
+      <main>
+        <ItemListContainer saludo={"Bienvenidos a mi ecommerce, GyL Importaciones"} />
+      </main>
+      <footer>
+
+      </footer>
     </>
   )
 }
---
+++ src/data/products.js
@@ -0,0 +1,20 @@
+const products = [
+    {
+        id: 1,
+        name: "Remera",
+        description: "Remera azul marino puma.",
+        precio: 1200
+    },
+    {
+        id: 2,
+        name: "Remera",
+        description: "Remera azul marino puma.",
+        precio: 1200
+    },
+    {
+        id: 3,
+        name: "Remera",
+        description: "Remera azul marino puma.",
+        precio: 1200
+    }
+]
