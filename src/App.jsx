ed2c6e266de5598630c22db8aa5e2074750c2988
LucasOlivera615 <lo.lucasolivera@gmail.com>
2025-10-22 12:02:58 -0300

    Añadido de estilos, nuevo componente que está en desarrollo, corrección de rutas en el componente NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './Components/NavBar/NavBar'
-import './App.css'
 import ItemListContainer from './Components/ItemListContainer/ItemListContainer'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
 
 function App() {
 
@@ -9,18 +10,17 @@ function App() {
 
   return (
     <>
-      <header className='container-fluid m-0 p-0'>
-        <h1>GyL Importaciones</h1>
-        <div className='col-12 mb-2'>
-          <NavBar />
-        </div>
-      </header>
-      <main className='container'>
-        <ItemListContainer destacados={"Productos destacados."} />
-      </main>
-      <footer>
-
-      </footer>
+      <BrowserRouter>
+        <header className='container-fluid m-0 p-0'>
+          <h1>GyL Importaciones</h1>
+          <div className='col-12 mb-2'>
+            <NavBar />
+          </div>
+        </header>
+        <main className='container'>
+          <ItemListContainer destacados={"Productos destacados."} />
+        </main>
+      </BrowserRouter>
     </>
   )
 }
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,5 +1,6 @@
 import './navbar.css'
 import logo from '../../Assets/img/logo-gyl-importaciones-1.2.png'
+import { Link } from 'react-router-dom'
 import CartWidget from '../CartWidget/CartWidget'
 
 const NavBar = () => {
@@ -8,13 +9,21 @@ const NavBar = () => {
     return (
         <nav className='container-fluid navBar'>
             <div className='col'>
-                <img src={logo} className='logo' alt="Logo de GyL importaciones" />
+                <Link>
+                    <img src={logo} className='logo' alt="Logo de GyL importaciones" />
+                </Link>
             </div>
             <div className='col'>
                 <ul className='categorias'>
-                    <li><a className='subrayado' href=''>Indumentaria</a></li>
-                    <li><a className='subrayado' href=''>Accesorios</a></li>
-                    <li><a className='subrayado' href=''>Estilo de vida</a></li>
+                    <li>
+                        <Link to="/category/indumentaria" className='subrayado'>Indumentaria</Link>
+                    </li>
+                    <li>
+                        <Link to="/category/accesorios" className='subrayado'>Accesorios</Link>
+                    </li>
+                    <li>
+                        <Link to="/category/estilo-de-vida" className='subrayado'>Estilo de vida</Link>
+                    </li>
                 </ul>
             </div>
             <div className='d-flex col h-100 justify-content-end'>
